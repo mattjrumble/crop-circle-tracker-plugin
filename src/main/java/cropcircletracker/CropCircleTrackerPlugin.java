3967276a1f791b89b7b82b873994903353522c43
mattjrumble <mattjrumble@gmail.com>
2022-09-05 20:25:11 +0100

    Add POST request logic

--- src/main/java/cropcircletracker/CropCircleTrackerPlugin.java
+++ src/main/java/cropcircletracker/CropCircleTrackerPlugin.java
@@ -1,6 +1,10 @@
 package cropcircletracker;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
 import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.Map;
 import javax.inject.Inject;
 
@@ -15,6 +19,7 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.task.Schedule;
+import okhttp3.*;
 
 import static net.runelite.api.ObjectID.CENTRE_OF_CROP_CIRCLE;
 
@@ -24,17 +29,44 @@ public class CropCircleTrackerPlugin extends Plugin
 {
 	private static final int CROP_CIRCLE_OBJECT = CENTRE_OF_CROP_CIRCLE;
 	private static final int POLLING_PERIOD_SECONDS = 30;
+	private static final String POST_URL = "http://127.0.0.1:8000/";
+	private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 	private static final Map<WorldPoint, CropCircle> MAPPING = CropCircle.mapping();
 
 	@Inject
 	private Client client;
+
+	@Inject
+	private OkHttpClient okHttpClient;
+
+	@Inject
+	private Gson gson;
 
 	private CropCircle lastCropCircle = null;
 
 	/* Send an HTTP POST request for a crop circle sighting. */
 	private void postSighting(CropCircle cropCircle, int world)
 	{
-		log.debug("Crop circle sighting on W" + world + ": " + cropCircle.getName());
+		Map<String, Object> data = new HashMap<>();
+		data.put("world", world);
+		data.put("location", cropCircle.getExternalId());
+		data.put("datetime", "...");
+		String json = gson.toJson(data);
+		log.debug("Posting sighting: {}", json);
+		Request request = new Request.Builder().url(POST_URL).post(RequestBody.create(JSON, json)).build();
+		okHttpClient.newCall(request).enqueue(new Callback()
+		{
+			@Override
+			public void onFailure(Call call, IOException e)
+			{
+				log.debug("Post failed: {}", e.getMessage());
+			}
+			@Override
+			public void onResponse(Call call, Response response)
+			{
+				response.close();
+			}
+		});
 	}
 
 	/* Determine if a crop circle is visible on the given WorldPoint. */
@@ -67,7 +99,7 @@ public class CropCircleTrackerPlugin extends Plugin
 			}
 			else
 			{
-				log.debug("Crop circle sighting removed: " + lastCropCircle.getName());
+				log.debug("Sighting removed: {}", lastCropCircle.getName());
 				lastCropCircle = null;
 			}
 		}
